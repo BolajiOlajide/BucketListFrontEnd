c4a1b8026e861f8b93c0d1a46de64ef0e5556dbc
Bolaji Olajide <bolaji.olajide@andela.com>
2017-04-07 09:47:05 +0100

    feature: complete homepage mockup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { MaterializeModule } from 'angular2-materialize';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    MaterializeModule,
     BrowserModule,
     FormsModule,
     HttpModule
